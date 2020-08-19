be18f80a33907e39f1d42410e1e4874995fddd47
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-08-19 14:40:56 -0400

    new version for pytest

--- test_hw4.py
+++ test_hw4.py
@@ -1,8 +1,35 @@
 from hw4 import main 
 import pytest
+import os.path
+from os import path
+import Queue
 
 def test_values():
-	assert main("Boston University") == "Boston University"
-	assert main("ECE") == "ECE"
-	assert main("Obama") == "Obama"
-	assert main("China") == "China"
+    main("Boston University")
+    assert videxist("Boston University") == True
+    main("ECE")
+    assert videxist("ECE") == True
+    main("Obama")
+    assert videxist("Obama") == True
+    main("China")
+    assert videxist("China") == True
+
+def test_queue():
+    Queue
+    assert videxist("Boston University") == True
+    assert videxist("ECE") == True
+    assert videxist("Dota") == True
+    assert videxist("2020") == True
+    assert videxist("United States") == True
+    assert videxist("China") == True
+    assert videxist("NBA") == True
+    assert videxist("Kobe") == True
+    assert videxist("Obama") == True
+    assert videxist("Boston") == True
+
+
+def videxist(keyword):
+    if path.exists(keyword + ".avi") == True:
+        return True
+    else:
+        return False
