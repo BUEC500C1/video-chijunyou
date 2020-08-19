0d55808046a7ffc038eb8d8fc36072bf94a068b2
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-08-19 13:06:07 -0400

    newversion

--- Queue.py
+++ Queue.py
@@ -1,18 +1,63 @@
-class Queue:
-	def __init__(self,inputlist=[]):
-		self.items = inputlist.copy()
+# author : noracnr@bu.edu
+import queue
+import multiprocessing
+import time
+import threading
+from hw4 import word2vid
 
-	def queueup(self, item):
-		self.items.append(item)
+num_worker_threads = 10
 
-	def queuedown(self):
-		return self.items.pop(0)
+class word2vid_task():
+    def __init__(self,keyword=""):
+        self.keyword = keyword
+        self.state = -1
+    def run(self):
+        word2vid(self.keyword)
 
-	def length(self):
-		return len(self.items)
+def source():
+  keys = [
+    "Boston University",
+    "ECE",
+    "Dota",
+    "2020",
+    "United States",
+    "China",
+    "NBA",
+    "Kobe",
+    "Obama",
+    "Boston",
+    "Beijing"
+  ]
+  return keys
 
-	def isEmpty(self):
-		if len(self.items) == 0:
-			return True
-		else:
-			return False
+def worker():
+  i = 0
+  while True:
+    item = q.get()
+    if item is None:
+      print("Break ! cuz item is None")
+      break
+    api = word2vid_task(item)
+    api.run()
+    i += 1
+    print("-----Task{0}----".format(i))
+    q.task_done()
+
+q = queue.Queue()
+threads = []
+for i in range(num_worker_threads):
+  t = threading.Thread(target=worker)
+  t.start()
+  threads.append(t)
+
+for item in source():
+  q.put(item)
+
+# block until all tasks are done
+q.join()
+
+# stop workers
+for i in range(num_worker_threads):
+  q.put(None)
+for t in threads:
+  t.join()
--- hw4.py
+++ hw4.py
@@ -3,13 +3,10 @@ import sys
 import configparser
 from makeimage import makeImage,makeImages
 from readtweets import gettweets
-from Queue import Queue
 import subprocess
 from keys  import getkeys
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/starthere/<keyword>')
 def word2vid(keyword):
 	keys = getkeys()
 	consumer_key = keys[0]
@@ -17,24 +14,16 @@ def word2vid(keyword):
 	access_key = keys[2]
 	access_secret = keys[3]
 
-	line = keyword
-	words = line.split(',')
-	q = Queue(words)
-	video_num = 0
-	print(q.isEmpty())
-	print(q.length())
-	while not q.isEmpty():
-		word = q.items[0]
-		image0 = "images/"+ str(video_num) + "-%01d.png"
-		video = word+".avi"
-		tweets = gettweets(word,consumer_key,consumer_secret,access_key,access_secret)
-		makeImages(video_num,tweets)
-		video_num = video_num+1
-		subprocess.call(['ffmpeg','-y', '-framerate', '.1', '-i', image0, video])
-		q.queuedown()
+
+	image0 = "images/"+ str(keyword) + "-%01d.png"
+	video = keyword+".avi"
+	tweets = gettweets(keyword,consumer_key,consumer_secret,access_key,access_secret)
+	makeImages(keyword,tweets)
+	subprocess.call(['ffmpeg','-y', '-framerate', '.1', '-i', image0, video])
+
 	return keyword
 
-def main(keyword = "Boston University,ECE"):
+def main(keyword = "obama"):
 	word2vid(keyword)
 	return keyword
 
